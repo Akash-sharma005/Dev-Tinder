f4cca6d6c26783b7a7a3c1c640d7798393bc27dc
Akash new <akashladwa64.in>
2026-05-10 07:56:18 +0530

    getAPIs

--- src/app.js
+++ src/app.js
@@ -17,8 +17,49 @@ app.post("/signup", async (req, res) => {
         res.status(400).send("Error in saving the user.." + err.message)
     }
     // console.log(req.body)
-    
+
 })
+
+//Now I'm going to fetch that users which are email id is equal to request
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        console.log(userEmail)
+        const user = await User.findOne({ emailId: userEmail })
+        if (!user) {
+            res.status(401).send("User not found in the database");
+        }
+        else {
+            res.send(user)
+        }
+
+
+        // if (users.length === 0) {
+        //     res.status(401).send("user not found");
+        // }
+        // else{
+        //     res.send(users);
+        // }
+    }
+    catch (err) {
+        res.status(401).send("Something went wrong");
+    }
+})
+
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users)
+    }
+    catch {
+        res.status(401).send("Something went wrong");
+    }
+})
+
+//feed API - GET/feed get all the users from the database
+// app.get("/feed",(req,res)=>{
+
+// })
 
 connectDB().
     then(() => {
